b287f1b3bdb8e48ca9ec58423d1baec803879b1a
Linus Jansson <00jansson@gmail.com>
2024-06-19 20:14:38 +0200

    Fixed lab2 again

--- PA1417/edutask/frontend/cypress/e2e/tests.cy.js
+++ PA1417/edutask/frontend/cypress/e2e/tests.cy.js
@@ -75,7 +75,7 @@ describe("Req 8", () => {
 
   //Describe
   describe("R8UC1", () => {
-    it("Main Succsess", () => {
+    it("Create todo", () => {
       //click the task with the title
       //type anything in todo
       cy.get('.inline-form > [type="text"]').type("anotherTestTask");
@@ -90,204 +90,116 @@ describe("Req 8", () => {
       cy.get(".todo-list > li").contains("anotherTestTask");
     });
 
-    it("Alternative", () => {
+    it("Dont create todo when input is empty", () => {
       cy.get('.inline-form > [type="submit"]').should("be.disabled");
     });
+    describe("R8UC2", () => {
+      it("Mark todo as done", () => {
+        //click .checker element within li with text testTask
+        cy.get(".todo-list > li")
+          .contains("testTask")
+          .parent()
+          .find(".checker")
+          .click();
+        //delay
+        cy.wait(1000);
+        //should have css  class checked
+        cy.get(".todo-list > li")
+          .contains("testTask")
+          .parent()
+          .find(".checker")
+          .should("have.class", "checked");
+      });
 
-    //
-    // //button is disabled when no todo text
-    // it('TEST: Button is disabled when no todo text', () => {
-    //     //click the task with the title
-    //     cy.get('.title-overlay').contains(title).click()
-    //     //type anything in todo
-    //     //ensure that text field is empty
-    //     cy.get('.inline-form > [type="text"]').clear()
-    //     cy.wait(1000)
-    //     cy.get('.inline-form > [type="submit"]').should('be.disabled')
-    // });
-    //
-    // //test click todo item, should be marked as done and strikethrough
-    // it('TEST: Test click todo item, should be marked as done and strikethrough', () => {
-    //     //click the task with the title
-    //     cy.get('.title-overlay').contains(title).click()
-    //     //type anything in todo
-    //     cy.wait(1000)
-    //
-    //     //click .checker element within li with text testTask
-    //     cy.get('.todo-list > li').contains("testTask").parent().find('.checker').click()
-    //     //delay
-    //     cy.wait(300)
-    //     cy.get('.todo-list > li').contains("testTask").parent().find('.checker').click()
-    //     //delay
-    //     cy.wait(1000)
-    //     //should have css  class checked
-    //     cy.get('.todo-list > li').contains("testTask").parent().find('.checker').should('have.class', 'checked')
-    // });
-    //
-    // //test click todo item, should be marked as done and strikethrough
-    // it('TEST: Test click todo item, should NOT BE done anymore', () => {
-    //     //click the task with the title
-    //     cy.get('.title-overlay').contains(title).click()
-    //     //type anything in todo
-    //     cy.wait(1000)
-    //
-    //     //click .checker element within li with text testTask
-    //     cy.get('.todo-list > li').contains("testTask").parent().find('.checker').click()
-    //
-    //     //delay
-    //     cy.wait(1000)
-    //     //should have css  class checked
-    //     cy.get('.todo-list > li').contains("testTask").parent().find('.checker').should('have.class', 'unchecked')
-    // });
-    //
-    //
-    // //test remove todo item
-    // describe('TEST: Test remove todo item', () => {
-    //     let taskName = "";
-    //
-    //     //before
-    //     before(() => {
-    //         for (var i = 0; i < 10; i++) {
-    //             taskName += chars[Math.floor(Math.random() * chars.length)];
-    //         }
-    //     })
-    //
-    //     beforeEach(() => {
-    //         //add new todo
-    //         cy.get('.title-overlay').contains(title).click()
-    //         //type anything in todo
-    //         cy.get('.inline-form > [type="text"]').type(taskName)
-    //         //click submit
-    //         //delay
-    //         cy.wait(1000)
-    //         cy.get('.inline-form > [type="submit"]').click()
-    //         //delay
-    //         cy.wait(1000)
-    //
-    //     })
-    //
-    //     it('test remove todo item', () => {
-    //         //click .remove element within li with text testTask
-    //         //delay
-    //         cy.get('.todo-list > li').contains(taskName).parent().find('.remover').click()
-    //         cy.get('.todo-list > li').contains(taskName).parent().find('.remover').click()
-    //         cy.wait(1000)
-    //         //should not contain taskname
-    //         cy.get('.todo-list > li').should('not.contain', taskName)
-    //
-    //     });
-    // });
-    //
-    //
-  });
+      it("Mark done todo item as not done", () => {
+        let todoId;
 
-  describe("R8UC2", () => {
-    it("Main Succsess", () => {
-      //click .checker element within li with text testTask
-      cy.get(".todo-list > li")
-        .contains("testTask")
-        .parent()
-        .find(".checker")
-        .click();
-      //delay
-      cy.wait(1000);
-      //should have css  class checked
-      cy.get(".todo-list > li")
-        .contains("testTask")
-        .parent()
-        .find(".checker")
-        .should("have.class", "checked");
-    });
+        cy.wrap(null).then(() => {
+          cy.request({
+            medthod: "GET",
+            url: `http://localhost:5000/tasks/byid/${taskId}`,
+          }).then((response) => {
+            console.log("THE TASK", response.body);
+            const todo = response.body.todos.filter(
+              (todo) => todo.description === "anotherOne",
+            );
+            console.log(todo);
+            todoId = todo[0]._id.$oid;
+          });
+        });
 
-    it("Alternative", () => {
-      let todoId;
+        cy.wrap(null).then(() => {
+          fetch(`http://localhost:5000/todos/byid/${todoId}`, {
+            method: "PUT",
+            headers: {
+              "User-Agent":
+                "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
+              Accept: "*/*",
+              "Accept-Language": "en-US,en;q=0.5",
+              "Accept-Encoding": "gzip, deflate, br, zstd",
+              Referer: "http://localhost:3000/",
+              "Cache-Control": "no-cache",
+              "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+              Origin: "http://localhost:3000",
+              Connection: "keep-alive",
+              "Sec-Fetch-Dest": "empty",
+              "Sec-Fetch-Mode": "cors",
+              "Sec-Fetch-Site": "same-site",
+              Priority: "u=1",
+            },
+            body: new URLSearchParams({
+              data: "{'$set': {'done': true}}",
+            }),
+          });
+        });
 
-      cy.wrap(null).then(() => {
-        cy.request({
-          medthod: "GET",
-          url: `http://localhost:5000/tasks/byid/${taskId}`,
-        }).then((response) => {
-          console.log("THE TASK", response.body);
-          const todo = response.body.todos.filter(
-            (todo) => todo.description === "anotherOne",
+        cy.get(".close-btn").click();
+
+        //delay
+        cy.wait(3000);
+        //reopen the task
+        cy.get(".title-overlay").contains(title).click();
+
+        //click .close-btn
+        cy.get(".close-btn").click();
+        cy.wait(3000);
+
+        //delay
+
+        //reopen the task
+        cy.get(".title-overlay").contains(title).click();
+
+        cy.wait(5555);
+
+        cy.get(".todo-list > li")
+          .contains("anotherOne")
+          .parent()
+          .find(".checker")
+          .click();
+
+        cy.wait(1000);
+
+        cy.get(".todo-list > li")
+          .contains("anotherOne")
+          .parent()
+          .find(".checker")
+          .should("not.have.class", "checked");
+
+        cy.get(".todo-list > li")
+          .contains("anotherOne")
+          .parent()
+          .find(".editable")
+          //should have text-decoration line-through
+          .should(
+            "not.have.css",
+            "text-decoration",
+            "line-through solid rgb(49, 46, 46)",
           );
-          console.log(todo);
-          todoId = todo[0]._id.$oid;
-        });
       });
-
-      cy.wrap(null).then(() => {
-        fetch(`http://localhost:5000/todos/byid/${todoId}`, {
-          method: "PUT",
-          headers: {
-            "User-Agent":
-              "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
-            Accept: "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            Referer: "http://localhost:3000/",
-            "Cache-Control": "no-cache",
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            Origin: "http://localhost:3000",
-            Connection: "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            Priority: "u=1",
-          },
-          body: new URLSearchParams({
-            data: "{'$set': {'done': true}}",
-          }),
-        });
-      });
-
-      cy.get(".close-btn").click();
-
-      //delay
-      cy.wait(3000);
-      //reopen the task
-      cy.get(".title-overlay").contains(title).click();
-
-      //click .close-btn
-      cy.get(".close-btn").click();
-      cy.wait(3000);
-
-      //delay
-
-      //reopen the task
-      cy.get(".title-overlay").contains(title).click();
-
-      cy.wait(5555);
-
-      cy.get(".todo-list > li")
-        .contains("anotherOne")
-        .parent()
-        .find(".checker")
-        .click();
-
-      cy.wait(1000);
-
-      cy.get(".todo-list > li")
-        .contains("anotherOne")
-        .parent()
-        .find(".checker")
-        .should("not.have.class", "checked");
-
-      cy.get(".todo-list > li")
-        .contains("anotherOne")
-        .parent()
-        .find(".editable")
-        //should have text-decoration line-through
-        .should(
-          "not.have.css",
-          "text-decoration",
-          "line-through solid rgb(49, 46, 46)",
-        );
     });
   });
-
   describe("R8UC3", () => {
-    it("Main Succsess", () => {
+    it("Remove todo", () => {
       cy.get(".todo-list > li")
         .contains("anotherOne")
         .parent()
